2f5442d2855f456a3bad041865f36b13c06deab2
Guilherme Azevedo da Gama <guilhermedagama2021100@gmail.com>
2023-07-09 19:17:39 -0300

    trocando url das redes

--- app/objects/redes.js
+++ app/objects/redes.js
@@ -1,14 +1,14 @@
 export const redes = [
     {
         name: "GitHub",
-        img: "http://localhost:3000/redes/github.png",
+        img: "https://amiibo-two.vercel.app/redes/github.png",
         creditos: "riajulislam",
         linkUser: "https://github.com/EsGui?tab=repositories",
         linkCreator: "https://www.flaticon.com/br/autores/riajulislam"
     },
     {
         name: "Linkedin",
-        img: "http://localhost:3000/redes/linkedin.png",
+        img: "https://amiibo-two.vercel.app/redes/linkedin.png",
         creditos: "riajulislam",
         linkUser: "https://www.linkedin.com/in/guilherme-azevedo-442170221/",
         linkCreator: "https://www.flaticon.com/br/autores/riajulislam"
